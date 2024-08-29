7dab109223c8dc3258d10e874135a54799b6625f
farhanzipp <farhanaji317@gmail.com>
2024-08-29 16:16:50 +0700

    fix: conflict

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,12 +4,9 @@ import { Serialize, SerializeInterceptor } from 'src/commons/interceptors/serial
 import { AuthService } from './auth.service';
 import { UserDto } from '../users/dto/user.dto';
 import { LoginDto } from './dto/auth.dto';
-<<<<<<< Updated upstream
-import { RefreshJwtGuard } from 'src/commons/guards/refresh.guard';
-=======
+
 import { RefreshJwtGuard } from '../../commons/guards/refresh.guard';
 import { JwtGuard } from 'src/commons/guards/jwt.guard';
->>>>>>> Stashed changes
 
 @Controller('auth')
 export class AuthController {
@@ -34,13 +31,8 @@ export class AuthController {
   }
 
   @Post('refresh')
-<<<<<<< Updated upstream
-  async refreshToken(@Request() req) {
-    return await this.authService.refreshToken(req.user);
-=======
   @UseGuards(RefreshJwtGuard)
   async refreshToken(@Request() request) {
     return await this.authService.refreshToken(request.user);
->>>>>>> Stashed changes
   }
 }
